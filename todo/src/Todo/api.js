d9f9cc9bb7d146d0f5e5334ddf7647059d9aef62
Ranga Raja R <r34rangaraja@gmail.com>
2023-11-19 19:38:46 +0530

    made api functtionality

--- todo/src/Todo/api.js
+++ todo/src/Todo/api.js
@@ -2,31 +2,71 @@ import React from 'react';
 import './todo.css'
 
 export default class Api extends React.Component{
-
+    
     constructor(props){
         super(props);
         this.state = {
-            todoList:[],
-            activeItem:{
-                id:null,
-                name:'',
+            todoList : [],
+            activeItem : {
+                task:'',
                 date:'',
                 completion_status:false,
             },
             editing:false,
         }
         this.fetchTasks = this.fetchTasks.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     };
+
+    handleChange(e){
+        console.log(e.target.value)
+        this.setState({
+            ...this.state,
+            activeItem:{
+                task:e.target.value,
+                date:new Date().toJSON().slice(0, 10),
+                completion_status:false,
+            }
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        console.log(JSON.stringify(this.state.activeItem));
+        const url = 'http://127.0.0.1:8000/todo-create/'
+        fetch(url, {
+            method:'POST',
+            headers:{
+                'Content-type':'application/json',
+            },
+            body:JSON.stringify(this.state.activeItem)
+        })
+        .then(response => {
+            this.fetchTasks();
+            this.setState({
+                ...this.state,
+                activeItem:{
+                    task:'',
+                    date:'',
+                    completion_status:false,
+                }
+            })
+        }).catch(function(error){
+            console.log('ERROR: ', error)
+        })
+    }
 
     fetchTasks(){
         const url = "http://127.0.0.1:8000/todo-list";
-        console.log('Fetching')
         fetch(url)
         .then(response => response.json())
-        .then(data => this.setState({
-            todoList:data
-        }))
-        // .then(data => {console.log(data)})
+        .then(data => (
+            this.setState({
+                ...this.state,
+                todoList:data,
+            })
+        ))
     }
 
     componentWillMount(){
@@ -38,15 +78,21 @@ export default class Api extends React.Component{
         return (
             <div>
                 <div>
-                    <input type='text'/>
-                    <button type='submit'>Add</button>
+                    <form onSubmit={this.handleSubmit}>
+                        <input onChange={this.handleChange} type='text' ref={this.state.inputRef}/>
+                        <button type='submit'>Add</button>
+                    </form>
                 </div>
                 <div>
-                <ul>
-                    {tasks.map(({task, index}) => (
-                        <li key={task.id} className='task-wrapper'>{task.name}</li>
-                    ))}
-                </ul>
+                    <div>
+                        {tasks.map(function(task, index){
+                            return(
+                                <div key={index} className='task-wrapper'>
+                                    <span>{task.task}</span>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         );
--- todoBackend/todoApi/models.py
+++ todoBackend/todoApi/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Todo(models.Model):
-    id = models.AutoField(primary_key=True)
     task = models.CharField(max_length=75)
     date = models.DateField()
     completion_status = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['task', 'date']
+        ordering = ['id', 'task', 'date']
 
     def __str__(self):
         return self.task
